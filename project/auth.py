5dce0e4971bc83251f070ee19b41a608cb24557d
Bilal Tosif <53641772+portwarrior@users.noreply.github.com>
2025-05-09 20:34:16 +1000

    User value is boolean now

--- project/auth.py
+++ project/auth.py
@@ -40,7 +40,7 @@ def signup_post():
     password = request.form.get('password')
 
     user = User.query.filter_by(email=email).all()
-    if user > 0: # if a user is found, we want to redirect back to signup page so user can try again
+    if user: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')  # 'flash' function stores a message accessible in the template code.
         app.logger.debug("User email already exists")
         return redirect(url_for('auth.signup'))
